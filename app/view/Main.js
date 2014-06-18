343a194e425a47f4edcef0dc539d6ffb7f3ccfca
avinash87 <avinash_020487@yahoo.com>
2014-06-18 13:47:44 +0530

    Testing

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,6 +1,6 @@
 Ext.define('AllInOneWorldSport.view.Main', {
     extend: 'Ext.tab.Panel',
-    xtype: 'main',
+    xtype: 'main', 
     requires: ["Ext.data.Store"],
     config: {
         tabBarPosition: 'bottom',

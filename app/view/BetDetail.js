fde34c1aeac86562dc0f3664e1005b06ef3c0bf4
avinash87 <avinash_020487@yahoo.com>
2014-06-18 23:35:00 +0530

    Changes

--- app/view/BetDetail.js
+++ app/view/BetDetail.js
@@ -25,11 +25,23 @@ Ext.define("AllInOneWorldSport.view.BetDetail",{
 				height: 120
 			},
 			options: [{
-				text: "To Win",
-				value: "to_win"
+				text: "1",
+				value: "1"
 			},{
-				text: "No Point Spread",
-				value: "no_point"
+				text: "2",
+				value: "2"
+			},
+			{
+				text: "3",
+				value: "3"
+			},
+			{
+				text: "4",
+				value: "4"
+			},
+			{
+				text: "5",
+				value: "5"
 			}]
 		}, {
 			xtype :"label",

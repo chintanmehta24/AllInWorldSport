16e08cd48cdc635d3ede85ec3d93912081c0f88d
avinash87 <avinash_020487@yahoo.com>
2014-07-04 15:25:14 +0530

    Function Added for the view

--- app/controller/MainMenu.js
+++ app/controller/MainMenu.js
@@ -47,7 +47,10 @@ Ext.define('AllInOneWorldSport.controller.MainMenu', {
             },
             "betdetail button[action=finalizeBet]": {
             	tap: "doConfirmBet"
-            }
+            },
+			"mybet button[action=friendListMyBets]":{
+				tap:"showBetFromFriend"
+			},
         }
     },
     
@@ -340,6 +343,16 @@ Ext.define('AllInOneWorldSport.controller.MainMenu', {
 		}
 		AllInOneWorldSport.Global.NavigationStack.push(mainPanel.getActiveItem());
 		mainPanel.animateActiveItem(mybet, {type: "slide", direction: "left", duration: 450});
-	}
+	},
 	
+	showBetFromFriend: function(){
+		var viewport = Ext.Viewport,
+			mainPanel = viewport.down("#mainviewport"),
+			mybetfromfriend = mainPanel.down("myBetfromfriends");
+		if(!mybetfromfriend){
+			mybetfromfriend = mainPanel.add({xtype: "myBetfromfriends"});
+		}
+		AllInOneWorldSport.Global.NavigationStack.push(mainPanel.getActiveItem());
+		mainPanel.animateActiveItem(mybetfromfriend, {type: "slide", direction: "left", duration: 450});
+	}	
 });
